ffea88cb6dd4e627e2e25d9fa9e16587a7fc2094
İsmail Kundakcı <ism.kundakci@hotmail.com>
2024-07-10 22:39:18 +0300

    Update Program.cs

--- src/WriteApi/Program.cs
+++ src/WriteApi/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var foo = 1
 builder.ConfigureJwtSettings();
 builder.ConfigureApiSettings();
 
